2a9e0856c5c15514679c323099845f16494ccb9b
Moyseys <moyseys.veroni@gmail.com>
2024-02-23 10:15:55 -0300

    Alteração nas regras de CORS

--- src/middlewares/configCors.js
+++ src/middlewares/configCors.js
@@ -7,13 +7,14 @@ export default (req, res, next) => {
 
   const { origin } = req.headers;
 
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    console.log(origin);
-  } else {
-    console.log('null');
-    res.header('Access-Control-Allow-Origin', null);
-  }
+  // if (allowedOrigins.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  //   console.log(origin);
+  // } else {
+  //   console.log('null');
+  //   res.header('Access-Control-Allow-Origin', null);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT');
   res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
   res.header('Access-Control-Max-Age', '86400');
